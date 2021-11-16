c23a000921424505e2452c5eee82062077519601
Carlos <arnau.carlos01@gmail.com>
2021-11-16 11:41:50 +0100

    New levels
    Uploading of the new levels desing

---
+++ Output/Assets/maps/Tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Tileset" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="../source/repos/B-platformer/Output/Assets/maps/Tileset.png" width="320" height="192"/>
+ <wangsets>
+  <wangset name="New World" type="mixed" tile="-1">
+   <wangcolor name="" color="#ff0000" tile="-1" probability="1"/>
+  </wangset>
+ </wangsets>
+</tileset>
